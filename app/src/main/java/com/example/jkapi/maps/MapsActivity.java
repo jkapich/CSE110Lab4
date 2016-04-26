cffbe92562b193bb33a3887c7b891639088676d7
jkapich <jkapich@gmail.com>
2016-04-25 18:50:44 -0700

    emulator working

--- app/src/main/java/com/example/jkapi/maps/MapsActivity.java
+++ app/src/main/java/com/example/jkapi/maps/MapsActivity.java
@@ -9,6 +9,7 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -79,7 +80,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ Manifest.permission.ACCESS_FINE_LOCATION},
+                    100);
+            Log.d("test1", "ins");
             return;
+        } else if (mMap != null){
+            Log.d("test2", "outs");
+            mMap.setMyLocationEnabled(true);
+
         }
         locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
 
